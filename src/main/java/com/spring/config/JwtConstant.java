c366489d69d0890a8517656e987e963ffc725225
lakshan102 <lakshan2725@gmail.com>
2024-12-09 17:47:14 +0530

    implement methods

---
+++ src/main/java/com/spring/config/JwtConstant.java
@@ -0,0 +1,6 @@
+package com.spring.config;
+
+public class JwtConstant {
+    public static final String SECRET_KEY = "oiugwfpiuewgrq28rpqiugpwiuegfpwieufgwufwiuegpwuerfpawfvlsavpwauegpwiuefgpwug";
+    public static final String JWT_HEADER = "Authorization";
+}
